746504642a3ca48ecad976e872893505b57c63a1
Oleg9791 <oleg9791@tut.by>
2021-03-11 21:08:05 +0300

    added

--- PROJECT PHP SQL 10.03.2021/add.php
+++ PROJECT PHP SQL 10.03.2021/add.php
@@ -1,5 +1,5 @@
 <?php
-
+// Проверка на заполненность всех полей данными
 if (empty($_POST["name"] or $_POST["name"] = "") or empty($_POST["login"] or $_POST["login"] = "")
     or empty($_POST["password"] or $_POST["password"] = "")) {
     echo "Все поля должны быть заполнены <br>";
@@ -9,11 +9,11 @@ if (empty($_POST["name"] or $_POST["name"] = "") or empty($_POST["login"] or $_P
 $name = $_POST["name"];
 $login = $_POST["login"];
 $password = $_POST["password"];
-
-
+// Хэширование пароля перед записью в базу данных с использованием соли
 $password = md5($password . "nbhgujl");
+// Подключение к базе данных
 $mysql = new mysqli("127.0.0.1", "root", "root", "user_bd", 3306);
-
+// Запрос к базе данных на сохранение введенных имени, логина и пароля в соответствующие поля таблицы базы данных
 $mysql->query("INSERT INTO `usersdata` (`name`, `login`, `password`) VALUES ('$name', '$login', '$password')");
 $mysql->close();
 
--- PROJECT PHP SQL 10.03.2021/login.php
+++ PROJECT PHP SQL 10.03.2021/login.php
@@ -1,4 +1,6 @@
 <?php
+// Проверка поля логина и пароля на наличие символов
+
 if (empty($_POST["login"] or $_POST["login"] = "")
     and empty($_POST["password"] or $_POST["password"] = "")) {
     echo "Все поля должны быть заполнены <br>";
@@ -7,14 +9,29 @@ if (empty($_POST["login"] or $_POST["login"] = "")
 }
 
 
-    $login = $_POST["login"];
-    $password = $_POST["password"];
+$login = $_POST["login"];
+$password = $_POST["password"];
 
-//    $password = md5($password . "nbhgujl");
-if ()
-    $mysql = new mysqli("127.0.0.1", "root", "root", "user_bd", 3306);
-    $mysql->query("select `password` from `usersdata` where `password` = '$password'");
+// Хэширование введенного в форму пароля до запроса в базу с добавлением в пароль "соли",эта функция(md5) уже не используется
 
-    $mysql->close();
+$password = md5($password . "nbhgujl");
+// Подключение к базе данных
+$mysql = new mysqli("127.0.0.1", "root", "root", "user_bd", 3306);
+// Запрос к базе данных на соответствие пароля и логина в соответствующих полях таблицы
+$query = $mysql->query("SELECT * FROM `usersdata` WHERE `password` = '$password' AND `login` = '$login'");
+// функция fetch_assoc создает ассоциативный массив в котором ключи являются полями в таблице базы данных
+$result = $query->fetch_assoc();
+//print_r($result);
+//die();
+// Проверка на корректность логина и пароля в базе данных
+if (empty($result)) {
 
-    header("Location: correct.html");
+    echo "Такой пользователь не существует";
+    exit();
+}
+// Выдает временную куку пользователю, который вошел на 30 секунд
+
+setcookie("id", $result["id_user"], time() + 30, "/");
+$mysql->close();
+
+header("Location: correct.html");
