59b688d56fdc3dc2a57fa1530f272675497867a0
Oleg9791 <oleg9791@tut.by>
2021-03-15 21:58:33 +0300

    add

--- 1/checkopros.php
+++ 1/checkopros.php
@@ -67,7 +67,7 @@
                         empty($_POST["q1"]) or empty($_POST["q2"]) or empty($_POST["q3"]) or empty($_POST["q4"])
                         or empty($_POST["q5"])
                     ) {
-                        echo "Выберите варианты ответа для всех вопросов <br> <a href='opros.php'>Вернуться к опросу</a>";
+                        echo "Выберите варианты ответа для всех вопросов <br> <a href='test.php'>Вернуться к опросу</a>";
                     };
                 }
                 ?>
