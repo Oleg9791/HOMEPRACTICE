ccd9fe5079e71abed2bba090e324c19eb394addd
Oleg9791 <oleg9791@tut.by>
2021-06-19 23:48:44 +0300

    added game CUBE and ROULETTE 19.06.2021

---
+++ 19.06.2021/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9e2c141d5cdde35c1df9533896537625e194864c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9e2c141d5cdde35c1df9533896537625e194864c',
+    ),
+  ),
+);
